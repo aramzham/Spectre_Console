94b59e07dc5521290eda97c2be36cd19d2b1b8cc
Aram Zhamkochyan <aramzham@hotmail.com>
2022-10-02 00:07:51 +0200

    live, progress, status

--- SpectreConsole.Live/Program.cs
+++ SpectreConsole.Live/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Spectre.Console;
 
+// live
 var table = new Table().Centered();
 
 await AnsiConsole.Live(table)
@@ -55,3 +56,55 @@ await AnsiConsole.Live(table)
     table.AddRow(new TableRow(new Markup[]{new("[red]4[/]"), new("[red]Armenia[/]") , new("[red]6[/]"), new("[red]1[/]"), new("[red]0[/]"), new("[red]5[/]"), new("[red]4:17[/]"), new("[red]3[/]")}));
     ctx.Refresh();
 });
+
+
+// progress
+await AnsiConsole.Progress()
+    .AutoRefresh(true) // Turn on auto refresh
+    .AutoClear(false)   // Do not remove the task list when done
+    .HideCompleted(false)   // Hide tasks as they are completed
+    .Columns(new ProgressColumn[] 
+    {
+        new TaskDescriptionColumn(),    // Task description
+        new ProgressBarColumn(),        // Progress bar
+        new PercentageColumn(),         // Percentage
+        new RemainingTimeColumn(),      // Remaining time
+        new SpinnerColumn(),            // Spinner
+    })
+    .StartAsync(async ctx =>
+    {
+        // Define tasks
+        var task1 = ctx.AddTask("[green]Reticulating splines[/]");
+        var task2 = ctx.AddTask("[green]Folding space[/]");
+
+        while (!ctx.IsFinished)
+        {
+            // Simulate some work
+            await Task.Delay(250);
+
+            // Increment
+            task1.Increment(1.5);
+            task2.Increment(0.5);
+        }
+    });
+
+
+// status
+await AnsiConsole.Status()
+    .Spinner(Spinner.Known.Dots)
+    .SpinnerStyle(Style.Parse("blue bold"))
+    .StartAsync("Thinking...", async ctx => 
+    {
+        // Simulate some work
+        AnsiConsole.MarkupLine("Doing some work...");
+        await Task.Delay(1000);
+        
+        // Update the status and spinner
+        ctx.Status("Thinking some more");
+        ctx.Spinner(Spinner.Known.Star);
+        ctx.SpinnerStyle(Style.Parse("green"));
+
+        // Simulate some work
+        AnsiConsole.MarkupLine("Doing some more work...");
+        await Task.Delay(2000);
+    });
